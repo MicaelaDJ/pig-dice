6250d5259c46538777c2a248e4163a16fbe87dc2
Micaela Jawor and Shawn Lunsford <micaela@>
2019-01-30 14:25:44 -0800

    redo objects and constructor for new dice roll and scoring functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,61 @@
 // var player
 // constructor for turns
 // prototype for rolling
-// prototype for holdig
+// prototype for holding
 // totaling score
 // changing turns
 // player win
 // restart game
+//this block of code sets up the points/
+/*
+function Player (totalPoints, roundPoints) {
+  this.totalPoints,
+  this.roundPoints
+}
+//this block of code sets variables
+var player1 = Player("player1");
+var player2 = Player("player2");
 
-var Player = (
-  currentRoll = 0
-  // tempScore
-  // totalScore
+function() {
+  // currentRoll: 0
+  // tempScore: 0,
+  // totalScore,
   // isTurn
-);
-
-Player.prototype.rollDice = function(currentRoll){
-  var currentRoll = 0;
-  alert("UHOH");
-  //return this.math.random(6)
 };
 
-function checkTurn(turn) {
-  if ( turn === true) {
-    game.player1.playerMove();
-  }
-}
+Player.rollDice = function(){
+  this.tempScore = alert("UHOH");
+  //return this.math.random(6)
+};
+*/
 
+Player.tempScore = 0;
+Player.totalScore = 0;
+
+function Player (tempScore, totalScore) {
+  this.tempScore = tempScore,
+  this.totalScore = totalScore;
+};
+
+Player.prototype.rollDice = function() {
+  let dice = Math.floor(Math.random() * 6) + 1;
+    $("#dice").text(dice);
+};
 
 // ### User Interface ### ----- -----
 $(document).ready(function() {
-
+  $("button#player1Roll").click(function(event) {
+    event.preventDefault();
+    $("#player1Roll").hide();
+    $("#player2Roll").show();
+    $("#player1score").text(Player.totalScore++);
+    Player.prototype.rollDice();
+  }); // end button#player1Roll.click
+  $("#player2Roll").click(function(event) {
+    event.preventDefault();
+    $("#player2Roll").hide();
+    $("#player1Roll").show();
+    $("#player2score").text(Player.totalScore++);
+    Player.prototype.rollDice();
+  }); // end button#player1Roll.click
 }); //end document.ready
