993a8e29d2343bfa929d86796abef3240a933027
Micaela Jawor and Shawn Lunsford <shawn@>
2019-01-31 10:07:43 -0800

    add basic css styling so eyes dont hurt, modify rollDice function to change player turn and reset players tempScore

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // ### Business Logic ### ----- -----
 // var player DONE
-// constructor for turns
+// thing for turns DONE
 // prototype for rolling DONE
-// prototype for holding
+// prototype for holding DONE
 // totaling score
 // changing turns
 // player win
@@ -19,42 +19,51 @@ var playerTwo = new Player();
 Player.prototype.rollDice = function() {
   this.dice = Math.floor(Math.random() * 6) + 1;
   $("#rollArea").text(this.dice);
+  console.log("Dice:" + this.dice);
   if ( this.dice === 1 ) {
     this.tempScore = 0;
+    console.log("One:" + this.dice)
     return false;
   } else if ( this.dice !== 1 ) {
     this.tempScore += this.dice;
+    console.log("NotOne:" + this.dice)
     return this.tempScore;
   } else {
-    return;
+    alert("Error: That wasn't supposed to happen.");
+    location.reload();
   };
-};
+}; // end of rollDice
 
-//Player.prototype.
+Player.prototype.holdDice = function() {
 
-
+}; // end of holdDice
 
 // ### User Interface ### ----- -----
 $(document).ready(function() {
   $("button#player1Roll").click(function(event) {
     event.preventDefault();
     var result = playerOne.rollDice();
-    if ( result = false ) {
+    if ( result === false ) {
       $("#player1Roll").hide();
       $("#player2Roll").show();
+      $("#player1Hold").hide();
+      $("#player2Hold").show();
+      $("#player1score").text(playerOne.tempScore);
     } else {
       $("#player1score").text(playerOne.tempScore);
     }
   }); // end button#player1Roll.click
-  $("#player2Roll").click(function(event) {
+  $("button#player2Roll").click(function(event) {
     event.preventDefault();
-    var result = playerOne.rollDice();
-    if ( result = false ) {
+    var result = playerTwo.rollDice();
+    if ( result === false ) {
       $("#player2Roll").hide();
       $("#player1Roll").show();
+      $("#player2Hold").hide();
+      $("#player1Hold").show();
+      $("#player2score").text(playerTwo.tempScore);
     } else {
       $("#player2score").text(playerTwo.tempScore);
     }
-    Player.prototype.rollDice();
   }); // end button#player1Roll.click
 }); //end document.ready
