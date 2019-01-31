57524f6fcf9adc5c6980ce1b885aac8531323a1c
Micaela Jawor and Shawn Lunsford <micaela@>
2019-01-31 11:36:08 -0800

    add reset button, modify score.text functions

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,6 @@ Player.prototype.rollDice = function() {
   this.dice = Math.floor(Math.random() * 6) + 1; // Perfect maths
   $("#rollArea").text(this.dice);
   if ( this.dice === 1 ) { // if dice is 1
-    this.tempScore = 0;
     return false; // return to playerX.rollDice result === false in UI
   } else if ( this.dice !== 1 ) {
     this.tempScore += this.dice;
@@ -43,6 +42,14 @@ Player.prototype.rollDice = function() {
 
 // ### User Interface ### ----- -----
 $(document).ready(function() {
+  $("button#resetGame").click(function(event) {
+    event.preventDefault();
+    $("#player1score").text(playerOne.tempScore = 0);
+    $("#player2score").text(playerTwo.tempScore = 0);
+    $("#player1total").text(playerOne.totalScore = 0);
+    $("#player2total").text(playerTwo.totalScore = 0);
+    $("#rollArea").text("TODO");
+  });
   $("button#player1Roll").click(function(event) {
     event.preventDefault();
     var result = playerOne.rollDice();
@@ -52,7 +59,7 @@ $(document).ready(function() {
       $("#player2Roll").show();
       $("#player1Hold").hide();
       $("#player2Hold").show();
-      $("#player1score").text(playerOne.tempScore); // if dice is 1, score is 0, print score 0
+      $("#player1score").text(playerOne.tempScore = 0); // if dice is 1, score is 0, print score 0
     } else { // if dice is not 1
       $("#player1score").text(playerOne.tempScore); // if dice is not 1, score is tempScore total, print tempScore until dice is 1 or hold
     };
@@ -60,10 +67,14 @@ $(document).ready(function() {
   $("button#player1Hold").click(function(event) {
     event.preventDefault();
     $("#player1total").text(playerOne.totalScore += playerOne.tempScore); // add turn score to total score on hold
-    playerOne.tempScore = 0; // reset tempScore to 0 to prevent 'hold' mash cheat
-    $("#player1score").text(playerOne.tempScore); // display that tempScore is now 0
+    $("#player1score").text(playerOne.tempScore = 0); playerOne.tempScore = 0;
     if (playerOne.totalScore >= 100) {
-      setTimeout(function() { alert("You win!");},100);
+      setTimeout(function() { alert("Player One Wins!");},100);
+      $("#player1score").text(playerOne.tempScore = 0);
+      $("#player2score").text(playerTwo.tempScore = 0);
+      $("#player1total").text(playerOne.totalScore = 0);
+      $("#player2total").text(playerTwo.totalScore = 0);
+      $("#rollArea").text("TODO");
     } else {
       //change player turn on hold
       $("#player1Roll").hide();
@@ -81,7 +92,7 @@ $(document).ready(function() {
       $("#player1Roll").show();
       $("#player2Hold").hide();
       $("#player1Hold").show();
-      $("#player2score").text(playerTwo.tempScore); // if dice is 1, score is 0, print score 0
+      $("#player2score").text(playerTwo.tempScore = 0); // if dice is 1, score is 0, print score 0
     } else { // if dice is not 1
       $("#player2score").text(playerTwo.tempScore); // if dice is not 1, score is tempScore total, print tempScore until dice is 1 or hold
     };
@@ -89,10 +100,14 @@ $(document).ready(function() {
   $("button#player2Hold").click(function(event) {
     event.preventDefault();
     $("#player2total").text(playerTwo.totalScore += playerTwo.tempScore); // add turn score to total score on hold
-    playerTwo.tempScore = 0; // reset tempScore to 0 to prevent 'hold' mash cheat
-    $("#player2score").text(playerTwo.tempScore); // display that tempScore is now 0
+    $("#player2score").text(playerTwo.tempScore = 0); // display that tempScore is now 0 to prevent 'hold' mashing
     if (playerTwo.totalScore >= 100) {
-      setTimeout(function() { alert("You win!");},100);
+      setTimeout(function() { alert("Player Two Wins!");},100);
+      $("#player1score").text(playerOne.tempScore = 0);
+      $("#player2score").text(playerTwo.tempScore = 0);
+      $("#player1total").text(playerOne.totalScore = 0);
+      $("#player2total").text(playerTwo.totalScore = 0);
+      $("#rollArea").text("TODO");
     } else {
     // change player turn on hold
       $("#player2Roll").hide();
